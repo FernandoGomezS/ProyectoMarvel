372d7b79dcd4efb8adba9351bf80cb12e577a11a
Fernando <fernandorgs1089@gmail.com>
2017-08-09 20:34:01 -0400

    integracion  lista y detalles de comic

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, NavController } from 'ionic-angular';
+import { comicsService } from '../../providers/comics-service/comics-service';
+import { DetailsPage } from '../../pages/details/details'
 
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  comics: any[] = [];
+
+  constructor(
+    public navCtrl: NavController,
+    public comicService: comicsService
+  ) { }
+
+  ionViewDidLoad() {
+    this.comicService.getcomics()
+      .then(data => {
+        this.comics = data.data.results;
+
+        this.comics = this.comics.map(item => {
+          //item.year = item.title.match(/\((.*)\)/).pop();  
+          item.year = (new Date(item.dates[0].date)).getFullYear();
+          item.src = item.thumbnail.path + "/portrait_small." + item.thumbnail.extension;
+          item.src2 = item.thumbnail.path + "/portrait_incredible." + item.thumbnail.extension;
+          return item;
+        });
+
+      })
+      .catch(error => {
+        console.error(error);
+      })
+
 
   }
-
+     openNavDetailsPage(comic) {      
+    this.navCtrl.push(DetailsPage, { comic: comic });
+  }
 }
---
+++ src/providers/comics-service/comics-service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+import {Md5} from 'ts-md5/dist/md5';
+
+@Injectable()
+export class comicsService {
+
+  constructor(
+    private http: Http
+  ) {}
+
+  getcomics() {
+    
+    var ts = 1;
+    var privateKey="edb8c96f8b36bd38946fb25207d7350de5ae3794";
+    var publicKey="8b4b8bdebf950524d49ab01de18e69ad";
+    var hash=Md5.hashStr(ts+privateKey+publicKey);
+    var limit =100;
+    return this.http.get('http://gateway.marvel.com/v1/public/comics?ts='+ts+'&apikey='+publicKey+'&hash='+hash+"&limit="+limit+"&orderBy=-onsaleDate")
+    .map(res => res.json())
+    .toPromise();
+    
+  }
+}
+
